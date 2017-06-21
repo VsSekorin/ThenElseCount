ef43ac13a1a1094fb08deb7354eadf32041df616
Vseslav Sekorin <VsSekorin@gmail.com>
2017-06-21 21:10:00 +0300

    Simple refactoring

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,33 +1,20 @@
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
 
 /**
   * @author VsSekorin
   */
 object Application {
 
-  val wordComment = List("/*", "*", "//")
+  val strWithIf = ".*\\Wif\\s*\\(.*"
+  val strWithElse = ".*\\s+else\\s+.*"
 
-  def numberIf(code: RDD[String]) =
-    code
-      .filter(_.matches(".*\\Wif\\s*\\(.*"))
-      .count
-
-  def numberElse(code: RDD[String]) =
-    code
-      .filter(_.matches(".*\\s+else\\s+.*"))
-      .count
-
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     val context = new SparkContext("local", "Then Else Count")
     val path = "files/**"
     val data = context textFile path cache
-    val code =
-      data
-        .filter(str => !wordComment.exists(str.trim startsWith))
-        .filter(_.length > 1)
-    val countIf = numberIf(code)
-    val countElse = numberElse(code)
+    val code = new Code(data)
+    val countIf = code numberOf strWithIf
+    val countElse = code numberOf strWithElse
     println(s"If: $countIf, Else: $countElse, Diff: ${countIf - countElse}")
   }
 }
---
+++ src/main/scala/Code.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.rdd.RDD
+
+/**
+  * @author VsSekorin
+  */
+class Code(text: RDD[String]) {
+
+  val code: RDD[String] =
+    text
+      .filter(str => !Code.wordComment.exists(str.trim startsWith))
+      .filter(_.length > 1)
+
+  def numberOf(pattern: String): Long =
+    code
+      .filter(_.matches(pattern))
+      .count
+}
+
+object Code {
+  val wordComment = List("/*", "*", "//")
+}
