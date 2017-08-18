56420f4160403498fdb3ed21705c4ac6efcbdc13
Vseslav Sekorin <VsSekorin@gmail.com>
2017-08-18 22:58:37 +0300

    Refactoring

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,20 +1,23 @@
 import org.apache.spark.SparkContext
 
 /**
-  * @author VsSekorin
-  */
+ * @author VsSekorin
+ */
 object Application {
-
-  val strWithIf = ".*\\Wif\\s*\\(.*"
-  val strWithElse = ".*\\s+else\\s+.*"
 
   def main(args: Array[String]): Unit = {
     val context = new SparkContext("local", "Then Else Count")
     val path = "files/**"
     val data = context textFile path cache
     val code = new Code(data)
-    val countIf = code numberOf strWithIf
-    val countElse = code numberOf strWithElse
-    println(s"If: $countIf, Else: $countElse, Diff: ${countIf - countElse}")
+    val countIf = code numberEquals "if"
+    val countElse = code numberEquals "else"
+    val countTernary = code numberContains "?"
+    println(
+      s"If: $countIf, " +
+      s"Else: $countElse, " +
+      s"Ternary: $countTernary, " +
+      s"Diff: ${countIf - countElse - countTernary}"
+    )
   }
 }
--- src/main/scala/Code.scala
+++ src/main/scala/Code.scala
@@ -1,19 +1,27 @@
 import org.apache.spark.rdd.RDD
 
 /**
-  * @author VsSekorin
-  */
+ * @author VsSekorin
+ */
 class Code(text: RDD[String]) {
 
-  val code: RDD[String] =
+  private val code: RDD[String] =
     text
+      .flatMap(_ split "(?=//)")
       .filter(str => !Code.wordComment.exists(str.trim startsWith))
-      .filter(_.trim.length > 1)
+      .flatMap(_ split "\\s+")
+      .flatMap(_ split "\\(")
+      .flatMap(_ split "\\{")
+      .flatMap(_ split "\\}")
+      .map(_ trim)
+      .filter(_.length > 0)
 
-  def numberOf(pattern: String): Long =
-    code
-      .filter(_ matches pattern)
-      .count
+  private def numberOf(predicate: String => Boolean): Long =
+    code filter predicate count
+
+  def numberEquals(word: String): Long = numberOf(_ equals word)
+
+  def numberContains(word: String): Long = numberOf(_ contains word)
 }
 
 object Code {
